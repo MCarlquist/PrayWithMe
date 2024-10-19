d6b6877f57cff440b6816806edc5644ee8c49640
Max Carlquist <maxcarlquist@icloud.com>
2024-10-19 23:42:41 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Pray With Me',
+  webDir: 'www'
+};
+
+export default config;
