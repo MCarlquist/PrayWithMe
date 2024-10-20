2eac5a0051b4a8e31972d6f091245907adbd8f55
Max Carlquist <maxcarlquist@icloud.com>
2024-10-20 11:42:11 +0200

    Finishing up registration

--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonModal } from '@ionic/angular';
+import { AlertController, IonModal } from '@ionic/angular';
 import { createClient } from '@supabase/supabase-js'
 import { environment } from 'src/environments/environment.prod';
 import { OverlayEventDetail } from '@ionic/core';
@@ -10,38 +10,78 @@ import { OverlayEventDetail } from '@ionic/core';
   styleUrls: ['./registration.page.scss'],
 })
 export class RegistrationPage implements OnInit {
-  name: string = '';
+  password: string = '';
   email: string = '';
 
   @ViewChild(IonModal) modal: IonModal | undefined;
-  
-  constructor() { }
+
+  constructor(private alertCtl: AlertController) { }
 
   ngOnInit() {
-    const supabase = createClient(environment.SupabaseUrl, environment.SupabaseAcessKey);
-    
-    
+
   }
 
   onDismiss(event: Event) {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
-    
-    if(ev.detail.role === 'confirm') {
+
+    if (ev.detail.role === 'confirm') {
       console.log('data', ev.detail.data);
-      
+
     }
   }
 
   cancel() {
     this.modal?.dismiss(null, 'cancel');
-    this.name = '';
+    this.password = '';
     this.email = '';
   }
 
-  confirm() {
-    console.log(this.email);
-    console.log(this.name);
-    this.modal?.dismiss({email: this.email, name: this.name}, 'confirm');
+  async confirm() {
+    const supabase = createClient(environment.SupabaseUrl, environment.SupabaseAcessKey);
+    const { data, error } = await supabase.auth.signUp({
+      email: this.email,
+      password: this.password,
+    });
+
+    if (error || (this.email === '' || this.password === '')) {
+
+      const alert = await this.alertCtl.create({
+        header: 'Oops, Something went wrong',
+        message: 'Please check again that everything is correct',
+        buttons: ['Close'],
+      });
+
+      await alert.present();
+
+    } else {
+      this.modal?.dismiss({ email: this.email, password: this.password }, 'confirm');
+    }
   }
+
+  async signIn() {
+    const supabase = createClient(environment.SupabaseUrl, environment.SupabaseAcessKey);
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: this.email,
+      password: this.password,
+    });
+
+    if (error || (this.email === '' || this.password === '')) {
+
+      const alert = await this.alertCtl.create({
+        header: 'Oops, Something went wrong',
+        message: 'Please check again that everything is correct',
+        buttons: ['Close'],
+      });
+
+      await alert.present();
+
+    } else {
+      this.modal?.dismiss({ email: this.email, password: this.password }, 'confirm');
+      console.log(data);
+      
+    }
+
+  }
+
 
 }
