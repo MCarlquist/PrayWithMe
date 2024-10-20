e3c390cb42f165dda8a70769bc8fa86fdb5101e7
Max Carlquist <maxcarlquist@icloud.com>
2024-10-20 10:43:19 +0200

    Begun Work on registration

--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { createClient } from '@supabase/supabase-js'
+import { environment } from 'src/environments/environment.prod';
+import { OverlayEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.page.scss'],
 })
 export class RegistrationPage implements OnInit {
+  name: string = '';
+  email: string = '';
 
+  @ViewChild(IonModal) modal: IonModal | undefined;
+  
   constructor() { }
 
   ngOnInit() {
+    const supabase = createClient(environment.SupabaseUrl, environment.SupabaseAcessKey);
+    
+    
+  }
+
+  onDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    
+    if(ev.detail.role === 'confirm') {
+      console.log('data', ev.detail.data);
+      
+    }
+  }
+
+  cancel() {
+    this.modal?.dismiss(null, 'cancel');
+    this.name = '';
+    this.email = '';
+  }
+
+  confirm() {
+    console.log(this.email);
+    console.log(this.name);
+    this.modal?.dismiss({email: this.email, name: this.name}, 'confirm');
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  SupabaseUrl: 'https://xmrlscfngybtrnsobagg.supabase.co',
+  SupabaseAcessKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtcmxzY2ZuZ3lidHJuc29iYWdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk0MTAwOTAsImV4cCI6MjA0NDk4NjA5MH0.SMjwRX2meM3fAPCz1QJl2V33iGDMiZJkysRVlaadSWk',
 };
